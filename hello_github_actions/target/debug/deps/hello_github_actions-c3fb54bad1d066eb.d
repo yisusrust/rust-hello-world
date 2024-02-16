23637a160b3fda1317865d056aa45d8828eee6e7
yisus <david@dailybot.com>
2024-02-15 21:52:33 -0500

    hello world code

---
+++ hello_github_actions/target/debug/deps/hello_github_actions-c3fb54bad1d066eb.d
@@ -0,0 +1,5 @@
+/Users/yisus/Documents/RustProjects/rust-hello-world/hello_github_actions/target/debug/deps/hello_github_actions-c3fb54bad1d066eb: src/main.rs
+
+/Users/yisus/Documents/RustProjects/rust-hello-world/hello_github_actions/target/debug/deps/hello_github_actions-c3fb54bad1d066eb.d: src/main.rs
+
+src/main.rs:
